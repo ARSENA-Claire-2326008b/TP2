c38087eb328dca463c77b67cb30f80202d58a8e0
Arsena <claire@A-F005606>
2025-01-21 17:12:30 +0100

    hey hey hey

---
+++ CheckPassFail.java
@@ -0,0 +1,14 @@
+public class CheckPassFail{
+	public static void main(String[] args){
+		int[] mark = {0, 49, 50, 51, 100};
+		System.out.println("mark est " + mark);
+
+		if (mark[] >= 50){
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAIL");
+		}
+		System.out.println("DONE");
+		
+	}
+}
