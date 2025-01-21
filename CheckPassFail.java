5fa58fc3ba211365cdbe7aa8308d23b4bb4a4c7a
b24015232 <heather.BURBECK@etu.univ-amu.fr>
2025-01-21 17:26:25 +0100

    correction

--- CheckPassFail.java
+++ CheckPassFail.java
@@ -2,13 +2,14 @@ public class CheckPassFail{
 	public static void main(String[] args){
 		int[] mark = {0, 49, 50, 51, 100};
 		System.out.println("mark est " + mark);
+		for(int i = 0; i < 6; i++) {
 
-		if (mark[] >= 50){
-			System.out.println("PASS");
-		} else {
-			System.out.println("FAIL");
-		}
+			if (mark[i] >= 50){
+				System.out.println("PASS");
+			} else {
+				System.out.println("FAIL");
+			}
 		System.out.println("DONE");
-		
+			}
 	}
 }
